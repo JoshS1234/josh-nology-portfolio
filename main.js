0631d32a15ae08b3aae7c7a93d2a70198d1bda2c
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-03-26 22:09:04 +0000

    added a sleep function using promises in main.js in order to reset CSS after spin effect has happened

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ if (!aboutMeImage) {
   throw new Error("Issue with about-me image");
 }
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 const handleImageClick = (e) => {
   if (e.target.classList.contains("about-me__image--rotating")) {
     e.target.classList.remove("about-me__image--rotating");
   } else {
     e.target.classList.add("about-me__image--rotating");
+    sleep(700)
+      .then(() => {
+        e.target.classList.remove("about-me__image--rotating");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 };
 
